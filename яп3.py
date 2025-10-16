9279bb031f80b56b0c06a859cf6254c9a5998aa4
Andrey Mescherin <Mesherina1978@yandex.ru>
2025-10-16 11:37:57 +0300

    третье задание

--- яп3.py
+++ яп3.py
@@ -5,3 +5,7 @@ print(squares)
 print(f"С помощью list comprehension получите список только чётных чисел из диапазона\n")
 even_numbers = [x for x in range(1, 20) if x % 2 == 0]
 print(even_numbers)
+
+words = ["python", "Java", "c++", "Rust", "go"]
+result = [word.upper() for word in words if len(word) > 3]
+print(result)
