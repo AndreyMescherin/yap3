60e8893330688742fde584467768c5d1c9eb115e
Andrey Mescherin <Mesherina1978@yandex.ru>
2025-10-16 11:40:11 +0300

    пятое задание

--- яп3.py
+++ яп3.py
@@ -28,3 +28,14 @@ class Countdown:
 
 for x in Countdown(5):
     print(x)
+
+
+
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
+
+for num in fibonacci(5):
+    print(num)
